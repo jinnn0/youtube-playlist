c95308578bdb2cc11ec1f700675828c77da4def9
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-12-09 17:38:15 +0000

    Updates

---
+++ precache-manifest.38fe208e2d33a6da3a855488e18db6d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af073060cf66b2e5d1e50f2d0bf850fd",
+    "url": "/youtube-playlist/index.html"
+  },
+  {
+    "revision": "2962a467b685f4aa65fd",
+    "url": "/youtube-playlist/static/js/2.69c3ab40.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/youtube-playlist/static/js/2.69c3ab40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8914f3ca9c0964fea3a0",
+    "url": "/youtube-playlist/static/js/main.44891bff.chunk.js"
+  },
+  {
+    "revision": "0587074c387e77e90c7a",
+    "url": "/youtube-playlist/static/js/runtime-main.53d7e1f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-youtube-playlist/precache-manifest.8c39cad754f123fe804e004f345fd5a3.js"
+  "/youtube-playlist/precache-manifest.38fe208e2d33a6da3a855488e18db6d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-youtube-playlist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtube-playlist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
