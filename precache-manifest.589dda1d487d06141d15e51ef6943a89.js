9315017f8123287eebeb5760847dc0023b2ca61d
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-12-09 17:33:47 +0000

    Updates

---
+++ precache-manifest.589dda1d487d06141d15e51ef6943a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac4008bed420e160a2848840432b0e7",
+    "url": "/react-youtube-playlist/index.html"
+  },
+  {
+    "revision": "93139089f8035d11d8d5",
+    "url": "/react-youtube-playlist/static/js/2.e20812dd.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/react-youtube-playlist/static/js/2.e20812dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2fc80e1c6e3208068b",
+    "url": "/react-youtube-playlist/static/js/main.1e98483c.chunk.js"
+  },
+  {
+    "revision": "1c132b2b8ec3dcb9082a",
+    "url": "/react-youtube-playlist/static/js/runtime-main.43e68b1f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-youtube-playlist/precache-manifest.589dda1d487d06141d15e51ef6943a89.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-youtube-playlist/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
